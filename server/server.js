5fcb46d073dfb91f619dec68623f975e0fe1946f
Thomas Harvey <thomasharvey1415@gmail.com>
2024-09-24 14:52:46 +0100

    Created database table in seed file. TH

--- server/server.js
+++ server/server.js
@@ -1,38 +1,36 @@
-import express, { request, response } from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import pg from "pg"
-dotenv.config()
+import express, { request, response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pg from "pg";
+dotenv.config();
 
-
-const app = express()
+const app = express();
 const dbConnectionSrting = process.env.DATABASE_URL;
 export const db = new pg.Pool({
-    connectionString: dbConnectionSrting,
-})
+  connectionString: dbConnectionSrting,
+});
 
+app.use(express.json());
+app.use(cors());
+const PORT = 8080;
+app.listen(PORT, function () {
+  console.log(`Server is running, I repeat Server is running on PORT ${8080}!`);
+});
 
-app.use(express.json())
-app.use(cors())
-app.listen(8080, function () {
-    console.log("Server is running, I repeat Server is running!")
-})
+app.get("/", (request, response) => {
+  response.json({
+    message: "Welcome",
+  });
+});
 
-app.get('/', (request, response) => {
-    response.json({
-        message: "Welcome"
-    })
-})
+app.get("/get-leaderboard", (request, response) => {
+  response.json({
+    message: "this is the leaderboard",
+  });
+});
 
-app.get('/get-leaderboard', (request, response) =>{
-    response.json({
-        message: "this is the leaderboard"
-    })
-})
-
-app.post('/send-data', (request, response) => {
-    response.json({
-        message: "sending data to the leaderboard"
-    })
-})
-
+app.post("/send-data", (request, response) => {
+  response.json({
+    message: "sending data to the leaderboard",
+  });
+});
