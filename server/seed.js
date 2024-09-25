e825216dfa537cb05d0df4aec6a30a40f623bf1f
Anu Heese <anuheese@gmail.com>
2024-09-25 12:43:20 +0100

    setting up database table, seeding data AH and MA

--- server/seed.js
+++ server/seed.js
@@ -3,5 +3,23 @@ import { db } from "./server.js";
 db.query(`CREATE TABLE IF NOT EXISTS leaderboard(
     id SERIAL PRIMARY KEY,
     name TEXT,
-    score SMALLINT
-);`);
+    addition_score SMALLINT,
+    subtraction_score SMALLINT,
+    multiplication_score SMALLINT,
+    division_score SMALLINT,
+    random_score SMALLINT
+)`);
+
+db.query(`CREATE TABLE IF NOT EXISTS leaderboard2(
+    id SERIAL PRIMARY KEY,
+    user_name TEXT,
+    addition_score SMALLINT,
+    subtraction_score SMALLINT,
+    multiplication_score SMALLINT,
+    division_score SMALLINT,
+    random_score SMALLINT
+)`);
+
+db.query(
+  `INSERT INTO leaderboard2 (name, addition_score) VALUES ('Musa','2' )`
+);
