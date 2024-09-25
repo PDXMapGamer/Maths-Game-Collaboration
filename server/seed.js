5c338a95876719b26adc9b42ed2bc68ef3ebc879
Anu Heese <anuheese@gmail.com>
2024-09-25 13:14:22 +0100

    database fix

--- server/seed.js
+++ server/seed.js
@@ -2,16 +2,6 @@ import { db } from "./server.js";
 
 db.query(`CREATE TABLE IF NOT EXISTS leaderboard(
     id SERIAL PRIMARY KEY,
-    name TEXT,
-    addition_score SMALLINT,
-    subtraction_score SMALLINT,
-    multiplication_score SMALLINT,
-    division_score SMALLINT,
-    random_score SMALLINT
-)`);
-
-db.query(`CREATE TABLE IF NOT EXISTS leaderboard2(
-    id SERIAL PRIMARY KEY,
     user_name TEXT,
     addition_score SMALLINT,
     subtraction_score SMALLINT,
@@ -21,5 +11,5 @@ db.query(`CREATE TABLE IF NOT EXISTS leaderboard2(
 )`);
 
 db.query(
-  `INSERT INTO leaderboard2 (name, addition_score) VALUES ('Musa','2' )`
+  `INSERT INTO leaderboard (user_name, addition_score) VALUES ('Musa','2' )`
 );
